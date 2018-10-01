0bc7037dd0e82511b764d80b46aeb2254ea18612
Luiz Gustavo da Silva Vasconcellos <iamtheluiz.dev@gmail.com>
2018-09-30 21:37:27 -0300

    Adicionado menu

---
+++ components/menu.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="nav-wrapper blue darken-4">
+        <a href="home.php" class="brand-logo left-align"><i class="material-icons">home</i><a>
+        <ul class="right">
+            <li><a href="home.php">Geral</a></li>
+			<li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+</nav>
